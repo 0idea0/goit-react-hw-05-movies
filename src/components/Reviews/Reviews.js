00cbee239753af71ffd08ee713b6a9c08b8999cf
KYRYLO VOITOVSKYI <kirill.voitovskiy@gmail.com>
2022-01-13 23:18:02 +0200

    add review

---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+import * as fetchApi from '../../service/api';
+import { useParams } from 'react-router';
+import s from './Reviews.module.css';
+
+const Reviews = () => {
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState();
+
+  useEffect(() => {
+    fetchApi
+      .fetchReviews(movieId)
+      .then(({ results }) => setReviews(results))
+      .catch(error => console.log(error));
+  }, [movieId]);
+
+  return (
+    <div className={s.ReviewsContainer}>
+      {reviews?.length > 0 ? (
+        <ul>
+          {reviews.map(({ id, author, content }) => {
+            return (
+              <li className={s.ReviewsItem} key={id}>
+                <p className={s.ReviewsTextContent}>Author: {author}</p>
+                <p>{content}</p>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p>We don`t have any Reviews for this movie.</p>
+      )}
+    </div>
+  );
+};
+
+export default Reviews;
