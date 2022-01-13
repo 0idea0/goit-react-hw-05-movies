a064ddf7d5b749a44526bf8d139c4fe5c5af126a
KYRYLO VOITOVSKYI <kirill.voitovskiy@gmail.com>
2022-01-13 23:16:32 +0200

    add navigation

---
+++ src/components/Navigation/Nav.js
@@ -0,0 +1,30 @@
+import { NavLink } from 'react-router-dom';
+import s from './Nav.module.css';
+
+export const Nav = () => {
+  return (
+    <div>
+      <ul className={s.Text}>
+        <li>
+          <NavLink
+            exact
+            to="/"
+            className={s.NavLink}
+            activeClassName={s.NavLink_Active}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/movies"
+            className={s.NavLink}
+            activeClassName={s.NavLink_Active}
+          >
+            Movies
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
